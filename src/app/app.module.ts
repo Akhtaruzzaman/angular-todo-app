6f0537032e71ec1bf44d14c567a20b00c7bb5929
Md. Akhtar Uzzaman <akhtaruzzaman05@hotmail.com>
2020-03-03 23:27:08 +0600

    upd

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './todo/index/index.component';
+import { CreateComponent } from './todo/create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
