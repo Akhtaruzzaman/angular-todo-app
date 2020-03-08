eb3be28a2b9eb5a8f66fc3ea9d8c3d6b3fe299af
Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2020-03-08 15:28:43 +0600

    upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './todo/index/index.component';
 import { CreateComponent } from './todo/create/create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
 import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +18,7 @@ import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgbModule,
     ToastrModule.forRoot({ positionClass: 'toast-top-right' }),
     ToastContainerModule
   ],
